40c7a8a58cfa23ca72932c455e53e2686ff5cf42
Shanaka Asiri <58134294+shanasiri@users.noreply.github.com>
2021-05-21 07:15:25 +0530

    Add files via upload

---
+++ Telephone/Phone.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Telephone
+{
+    public partial class Phone : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=H:\C#\Telephone\Telephone\Phone.mdf;Integrated Security=True");
+
+        public Phone()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Clear();
+            textBox3.Text = "";
+            textBox4.Clear();
+            comboBox1.SelectedIndex = -1;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("INSERT INTO Mobiles (firstname, lastname, mobile, email, category) VAlUES ('" + textBox1.Text + "', '" + textBox2.Text + "', '" + textBox3.Text + "', '" + textBox4.Text + "', '" + comboBox1.Text + "')", con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Successfully Saved...!");
+            Display();
+        }
+
+        void Display()
+        {
+            SqlDataAdapter sda = new SqlDataAdapter("SELECT * FROM MOBILES", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            dataGridView1.Rows.Clear();
+
+            foreach (DataRow item in dt.Rows)
+            {
+                int n = dataGridView1.Rows.Add();
+
+                dataGridView1.Rows[n].Cells[0].Value = item["firstname"].ToString();
+                dataGridView1.Rows[n].Cells[1].Value = item[1].ToString();
+                dataGridView1.Rows[n].Cells[2].Value = item[2].ToString();
+                dataGridView1.Rows[n].Cells[3].Value = item[3].ToString();
+                dataGridView1.Rows[n].Cells[4].Value = item[4].ToString();
+            }
+        }
+
+        private void Phone_Load(object sender, EventArgs e)
+        {
+            Display();
+        }
+
+        private void dataGridView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = dataGridView1.SelectedRows[0].Cells[0].Value.ToString();
+            textBox2.Text = dataGridView1.SelectedRows[0].Cells[1].Value.ToString();
+            textBox3.Text = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
+            textBox4.Text = dataGridView1.SelectedRows[0].Cells[3].Value.ToString();
+            comboBox1.Text = dataGridView1.SelectedRows[0].Cells[4].Value.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("DELETE FROM Mobiles WHERE (mobile = '"+textBox3.Text+"')", con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Delete Successfully...!");
+            Display();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("UPDATE Mobiles SET firstname = '"+textBox1.Text+ "', lastname = '" + textBox2.Text + "', mobile = '" + textBox3.Text + "', email = '" + textBox4.Text + "', category = '" + comboBox1.Text + "' WHERE (mobile = '" + textBox3.Text + "')", con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Update Successfully...!");
+            Display();
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            SqlDataAdapter sda = new SqlDataAdapter("SELECT * FROM MOBILES WHERE mobile like '"+textBox5.Text+"%'", con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            dataGridView1.Rows.Clear();
+
+            foreach (DataRow item in dt.Rows)
+            {
+                int n = dataGridView1.Rows.Add();
+
+                dataGridView1.Rows[n].Cells[0].Value = item["firstname"].ToString();
+                dataGridView1.Rows[n].Cells[1].Value = item[1].ToString();
+                dataGridView1.Rows[n].Cells[2].Value = item[2].ToString();
+                dataGridView1.Rows[n].Cells[3].Value = item[3].ToString();
+                dataGridView1.Rows[n].Cells[4].Value = item[4].ToString();
+            }
+        }
+    }
+}
